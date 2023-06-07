23313a34409c2b1d3c6437391cfed81f27bd0692
Andrew1412Belous <andrew.belous1412@gmail.com>
2023-06-07 18:42:38 +0300

    add logger, base controller

---
+++ src/logger/logger.service.ts
@@ -0,0 +1,27 @@
+import { ILogObj, ISettingsParam, Logger } from 'tslog'
+
+export class LoggerService {
+  private logger: Logger<ILogObj>
+
+  constructor() {
+    this.logger = new Logger({
+      prettyLogTemplate: "{{yyyy}}-{{mm}}-{{dd}} {{hh}}:{{MM}}:{{ss}}:{{ms}}\t{{logLevelName}} {{name}}",
+      displayInstanceName: false,
+      displayLoggerName: false,
+      displayFilePath: 'hidden',
+      displayFunctionName: false,
+    } as ISettingsParam<ILogObj>)
+  }
+
+  log(...args: unknown[]): void {
+    this.logger.info(...args)
+  }
+
+  error(...args: unknown[]): void {
+    this.logger.error(...args)
+  }
+
+  warn(...args: unknown[]): void {
+    this.logger.warn(...args)
+  }
+}
